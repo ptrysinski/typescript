9886192666aec8f92a93304ee9b2e8927f4cdf23
Piotr Trysiński <ptrysinski@gmail.com>
2019-04-24 11:48:51 +0200

    basics

---
+++ basics.ts
@@ -0,0 +1,12 @@
+let zrobione: boolean = true
+let liczba: number = 123
+let text: string = 'to jest test'
+
+const tablicaLiczba: number[] = [1,2,3]
+const tablicaLiczba2: Array<number>=[3,2,1]
+
+const tablicaStringów: Array<string>=['a','b']
+
+let krotka: [number, boolean] = [1, false]
+let krotka1: [string, boolean[]] = ['test', [true, false, true]]
+let krotka2: [[number, boolean], [string, boolean[]]] = [krotka, krotka1]
---
+++ test.ts
@@ -0,0 +1,10 @@
+console.log('test')
+function add(a, b) {
+    a=a+1
+    b=b+1
+    return a+b    
+}
+
+const c = add(7,9)
+const b = add('12', 7)
+const d = add(null, '12')
